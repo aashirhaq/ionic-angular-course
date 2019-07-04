09e4df9f034cc493c6543051f14f1cdd0e157954
Aashir Haque <aashirulhaque@gmail.com>
2019-07-04 18:28:26 +0500

    video no 5 completed

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  text = 'Default starting text';
+
+  onChangeText() {
+    this.text = 'Changed!';
+  }
 
 }
